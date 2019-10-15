dbbc442f237d4d9803eb621e71af996d8d408b99
dragonsploder <dragonsploder@gmail.com>
2019-10-15 10:22:13 -0700

    Started on input parser

---
+++ parser.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+#include "KingServent.h"
+
+using namespace std;
+
+string getInput(){
+    string rawInput;
+    int command = 0;
+    do {
+        cin >> rawInput;
+    } while (command == 0);
+}
