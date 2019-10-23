2c66248983ee4d9d3887437288661635ca2b8f68
dragonsploder <dragonsploder@gmail.com>
2019-10-22 21:33:20 -0700

    Spelling mistakes

--- data.cpp
+++ data.cpp
@@ -86,15 +86,15 @@ string commands[9][10] = {
 // 0 is the defalt
 roomType roomTypes[] = {
     {"Unused", "", false}, // 0
-    {"King's bedroom", "An almost sacred room only to be tread in the most neccisary of times.", true}, // 1
-    {"kitchen", "A room full of wondrous smells were food fit for kings (of course) is made.", true}, // 2
+    {"King's bedroom", "An almost sacred room only to be tread in the most necessary of times.", true}, // 1
+    {"kitchen", "A room full of wondrous smells where food fit for kings (of course) is made.", true}, // 2
     {"storeroom", "A stuffed room which houses everything from food to spare sheets.", true}, // 3
     {"dinning room", "A large room with an even larger table in the middle.", true}, // 4
-    {"servents corrder", "A cramped room in which you can sleep, eat or even just take a break without getting yelled at.", true}, // 5
+    {"servants corridor", "A cramped room in which you can sleep, eat or even just take a break without getting yelled at.", true}, // 5
     {"game room", "A cheery room were the sounds of chatter and fun echo off the walls.", false}, // 6
-    {"music room", "A loud room were songs, melodies, balleds can be heard.", false}, // 7
+    {"music room", "A loud room were songs, melodies, and ballads can be heard.", false}, // 7
     {"library", "A quiet, calming room filled with shelves of books.", false}, // 8
-    {"armory", "A somber room filled with polished swords, sheilds and other metal objects.", false}, // 9
-    {"treasure room", "A windowless room with mountiens of gold, piles of loot and heaps of riches.", false}, // 10
+    {"armory", "A somber room filled with polished swords, shields and other metal objects.", false}, // 9
+    {"treasure room", "A windowless room with mountains of gold, piles of loot and heaps of riches.", false}, // 10
     {"hall", "A long narrow passage used to connect more important rooms.", false} // 11
 };
